d7518866f23d94390d79455d5717e9018f4837fe
UT02004 <UT02004@UNICOMTIC74>
2024-10-11 14:04:37 +0530

    Add project files.

---
+++ BookRentalManagementSystem_V1/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    public class Book
+    {
+        private string bookId {  get; set; }
+        public string GetbookId() 
+
+        { return bookId; }
+
+        public void SetbookId(string value)
+        {
+            bookId = value;
+        }
+
+
+
+        private string title { get; set; }
+        public string Gettitle() 
+        
+        { return title; }
+
+        public void Settitle(string value)
+        {
+            title = value;
+        }
+
+
+        private
+
+    }
+}
